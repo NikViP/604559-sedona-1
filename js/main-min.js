48c6f113941e0abd470cbe65d78ef93a1df56dd8
NikViP <p11591@mail.ru>
2018-09-04 09:55:40 +0500

    Проект готов к проверке

---
+++ js/main-min.js
@@ -0,0 +1,1 @@
+document.querySelector(".invitation-modal").classList.toggle("invitation-modal-show");document.querySelector(".button-invitation").addEventListener("click",function(){document.querySelector(".invitation-modal").classList.toggle("invitation-modal-show");});
